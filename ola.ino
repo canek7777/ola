c72433eb956403685aa7ac3e181c2be4ca27c940
canek7777 <jasmindelangelc734@gmail.com>
2023-02-23 19:31:39 -0600

    Se agrega el Proyeco

---
+++ ola.ino
@@ -0,0 +1,14 @@
+
+
+//ola 
+
+void setup() {
+  Serial.begin(115200);
+  Serial.print("Ejemplo setup");
+  Serial.println("ejemplo siguiente");
+}
+
+void loop() {
+    Serial.println("ejemplo siguiente");
+    delay(2000);
+}
